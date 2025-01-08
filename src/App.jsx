e87e53303b58a1cc8bcdea675222a32ca8c6eb60
eynow1159 <eynow1159@gmail.com>
2025-01-08 16:27:04 +0000

    ✨라우팅, ESLint, Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './styles/globalstyles.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './layout/root-layout';
 import HomePage from './pages/homepage';
+import NotFoundPage from './pages/notfoundpage';
+import LoginPage from './pages/loginpage';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +13,39 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage/>
+      },
+      {
+        path: 'login',
+        element: <LoginPage/>
+      },
+      {
+        path: 'signin',
+        // element: <HomePage/>
+      },
+      {
+        //파트너드 찾기
+        path: 'find',
+        // element: <HomePage/>
+      },
+      {
+        path: 'collaboration',
+        // element: <HomePage/>
+      },
+      {
+        path: 'project',
+        // element: <HomePage/>
+      },
+      {
+        path: 'community',
+        // element: <HomePage/>
+      },
+      {
+        path: 'mypage',
+        // element: <HomePage/>
+      },
+      {
+        path: '*', // 404 에러를 처리하는 와일드카드 경로
+        element: <NotFoundPage /> // 404 페이지
       }
       //아래에 추가해주세요.
     ]
---
+++ src/pages/notfoundpage.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () =>{
+    return(
+        <h1>
+            잘못된 경로입니다.
+        </h1>
+    )
+}
+
+export default NotFoundPage;
